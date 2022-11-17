4cbb708013eee8df83f69fbce5ec83e9be195381
Daniel <101649846+Rhoyndes@users.noreply.github.com>
2022-11-17 18:56:12 +0100

    initial commit

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,6 @@
 import { ChangeSide } from './components/changeSide.js';
 import { Calendar } from './components/calendar.js';
 import { AddToDashboard } from './components/addToDashboard.js';
-import { BrowserCheck } from './components/browserCheck.js';
 ChangeSide;
 Calendar;
 AddToDashboard;
-BrowserCheck;
